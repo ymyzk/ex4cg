9acb5117e8a6942d88f03f8738286aaba2532d2b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-10-22 22:10:55 +0900

    Update Vrml

--- cg/vrml.py
+++ cg/vrml.py
@@ -7,6 +7,9 @@ from enum import Enum
 import numpy as np
 
 
+DOUBLE = np.float64
+
+
 class Status(Enum):
     material = 1
     point = 2
@@ -36,31 +39,24 @@ class Vrml(object):
             if len(items) == 0:
                 continue
 
-            if items[0] in ('diffuseColor', 'specularColor',
-                            'ambientIntensity', 'shininess',):
+            if status is Status.point and len(items) == 3:
+                l = [float(i.replace(',', '')) for i in items]
+                self.points.append(np.array(l, dtype=DOUBLE))
+            elif status is Status.index and len(items) == 4:
+                self.indexes.append(
+                    [int(i.replace(',', '')) for i in items[:3]])
+            elif status is Status.material:
                 if items[0] == 'diffuseColor':
                     self.diffuse_color = np.array(
-                        tuple(map(float, items[1:4])), dtype=np.float64)
+                        tuple(map(float, items[1:4])), dtype=DOUBLE)
                 elif items[0] == 'specularColor':
                     self.specular_color = np.array(
-                        tuple(map(float, items[1:4])), dtype=np.float64)
+                        tuple(map(float, items[1:4])), dtype=DOUBLE)
                 elif items[0] == 'shininess':
                     self.shininess = float(items[1])
                 elif items[0] == 'ambientIntensity':
                     self.ambient_intensity = float(items[1])
-            elif items[0] == 'point':
+            if items[0] == 'point':
                 status = Status.point
-                continue
-            elif items[0] == 'coordIndex':
+            if items[0] == 'coordIndex':
                 status = Status.index
-                continue
-
-            if (status is Status.point and
-                    len(items) == 3 and
-                    items[2].endswith(',')):
-                l = [float(i.replace(',', '')) for i in items]
-                self.points.append(np.array(l, dtype=np.float64))
-
-            if status is Status.index and len(items) == 4:
-                self.indexes.append(
-                    [int(i.replace(',', '')) for i in items[:3]])
