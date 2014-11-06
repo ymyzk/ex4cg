06b439997c4ce8bba5bf3dff80110618234b5fc0
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-11-07 00:09:42 +0900

    Update test runner

--- scripts/test.sh
+++ scripts/test.sh
@@ -18,9 +18,11 @@ fi
 echo
 
 echo '===== task2.py ====='
+start_time=`date +%s`
 for vrml in $vrml_files; do
   echo $vrml
-  python task2.py $vrml > task2.py.log 2>&1
+  ppm=${vrml%.wrl}_2.ppm
+  python task2.py -o $ppm $vrml > task2.py.log 2>&1
   if [ $? -eq 0 ]; then
     echo 'Success'
   else
@@ -29,5 +31,92 @@ for vrml in $vrml_files; do
     exit 1
   fi
 done
+echo
+end_time=`date +%s`
+seconds=`expr $end_time - $start_time`
+echo "Finished $seconds [sec]"
+echo
+
+echo '===== task4_1.py ====='
+start_time=`date +%s`
+for vrml in $vrml_files; do
+  echo $vrml
+  ppm=${vrml%.wrl}_4_1_f.ppm
+  profile=${vrml%.wrl}_4_1_f.json
+  python task4_1.py -s flat -p $profile -o $ppm $vrml > task4_1.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Flat)'
+  else
+    echo 'Error (Flat)'
+    cat task4_1.py.log
+    exit 1
+  fi
+  ppm=${vrml%.wrl}_4_1_g.ppm
+  profile=${vrml%.wrl}_4_1_g.json
+  python task4_1.py -s gouraud -p $profile -o $ppm $vrml > task4_1.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Gouraud)'
+  else
+    echo 'Error (Gouraud)'
+    cat task4_1.py.log
+    exit 1
+  fi
+  ppm=${vrml%.wrl}_4_1_p.ppm
+  profile=${vrml%.wrl}_4_1_p.json
+  python task4_1.py -s phong -p $profile -o $ppm $vrml > task4_1.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Phong)'
+  else
+    echo 'Error (Phong)'
+    cat task4_1.py.log
+    exit 1
+  fi
+done
+echo
+end_time=`date +%s`
+seconds=`expr $end_time - $start_time`
+echo "Finished $seconds [sec]"
+echo
+
+echo '===== task4_2.py ====='
+start_time=`date +%s`
+for vrml in $vrml_files; do
+  echo $vrml
+  ppm=${vrml%.wrl}_4_2_f.ppm
+  profile=${vrml%.wrl}_4_2_f.json
+  python task4_2.py -s flat -p $profile -o $ppm $vrml > task4_2.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Flat)'
+  else
+    echo 'Error (Flat)'
+    cat task4_2.py.log
+    exit 1
+  fi
+  ppm=${vrml%.wrl}_4_2_g.ppm
+  profile=${vrml%.wrl}_4_2_g.json
+  python task4_2.py -s gouraud -p $profile -o $ppm $vrml > task4_2.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Gouraud)'
+  else
+    echo 'Error (Gouraud)'
+    cat task4_2.py.log
+    exit 1
+  fi
+  ppm=${vrml%.wrl}_4_2_p.ppm
+  profile=${vrml%.wrl}_4_2_p.json
+  python task4_2.py -s phong -p $profile -o $ppm $vrml > task4_2.py.log 2>&1
+  if [ $? -eq 0 ]; then
+    echo 'Success (Phong)'
+  else
+    echo 'Error (Phong)'
+    cat task4_2.py.log
+    exit 1
+  fi
+done
+echo
+end_time=`date +%s`
+seconds=`expr $end_time - $start_time`
+echo "Finished $seconds [sec]"
+echo
 
 exit 0
